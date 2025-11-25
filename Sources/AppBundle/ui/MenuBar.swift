nikitabobko/AeroSpace

Do not focus on most recently used app when workspace focus is changed by mouse click

Sources/AppBundle/ui/MenuBar.swift
@@ -16,7 +16,7 @@ public func menuBar(viewModel: TrayMenuModel) -> some Scene { // todo should it 
             ForEach(viewModel.workspaces, id: \.name) { workspace in
                 Button {
                     Task {
-                        try await runSession(.menuBarButton, token) { _ = Workspace.get(byName: workspace.name).focusWorkspace() }
+                        try await runSession(.menuBarButton, token) { _ = Workspace.get(byName: workspace.name).focusWorkspace(source: .keyboardShortcut) }
                     }
                 } label: {
                     Toggle(isOn: .constant(workspace.isFocused)) {

yuliswe/Devs-Best-AeroSpace-fork

Sources/AppBundle/ui/MenuBar.swift
@@ -16,7 +16,7 @@ public func menuBar(viewModel: TrayMenuModel) -> some Scene { // todo should it 
             ForEach(viewModel.workspaces, id: \.name) { workspace in
                 Button {
                     Task {
-                        try await runLightSession(.menuBarButton, token) { _ = Workspace.get(byName: workspace.name).focusWorkspace() }
+                        try await runLightSession(.menuBarButton, token) { _ = Workspace.get(byName: workspace.name).focusWorkspace(source: .keyboardShortcut) }
                     }
                 } label: {
                     Toggle(isOn: .constant(workspace.isFocused)) {