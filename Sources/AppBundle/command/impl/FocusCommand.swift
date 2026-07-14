nikitabobko/AeroSpace

Update FocusCommand's floating window logic for more stable sorting

Sources/AppBundle/command/impl/FocusCommand.swift
@@ -120,22 +120,34 @@ struct FocusCommand: Command {
     defer {
         mruBefore?.markAsMostRecentChild()
     }
+    
+    // Get the first tiling window in the workspace as the target
+    guard let firstTilingWindow = workspace.rootTilingContainer.allLeafWindowsRecursive.first else {
+        return []
+    }
+    guard let tilingParent = firstTilingWindow.parent as? TilingContainer else {
+        return []
+    }
+    
     var _floatingWindows: [FloatingWindowData] = []
     for window in workspace.floatingWindows {
         let center = try await window.getCenter() // todo bug: we shouldn't access ax api here. What if the window was moved but it wasn't committed to ax yet?
         guard let center else { continue }
-        // todo bug: what if there are no tiling windows on the workspace?
-        guard let target = center.coerceIn(rect: workspace.workspaceMonitor.visibleRectPaddedByOuterGaps)?
-            .findIn(tree: workspace.rootTilingContainer, virtual: true) else { continue }
-        guard let targetCenter = try await target.getCenter() else { continue }
-        guard let tilingParent = target.parent as? TilingContainer else { continue }
-        let index = center.getProjection(tilingParent.orientation) >= targetCenter.getProjection(tilingParent.orientation)
-            ? target.ownIndex.orDie() + 1
-            : target.ownIndex.orDie()
+        
+        // Always use the first tiling window's parent container
+        let index = firstTilingWindow.ownIndex.orDie() + 1 // Insert after the first tiling window
         let data = window.unbindFromParent()
         _floatingWindows.append(FloatingWindowData(window: window, center: center, parent: tilingParent, adaptiveWeight: data.adaptiveWeight, index: index))
     }
-    let floatingWindows: [FloatingWindowData] = _floatingWindows.sortedBy { $0.center.getProjection($0.parent.orientation) }.reversed()
+    let floatingWindows: [FloatingWindowData] = _floatingWindows.sorted { 
+        let projection1 = $0.center.getProjection($0.parent.orientation)
+        let projection2 = $1.center.getProjection($1.parent.orientation)
+        if abs(projection1 - projection2) < 0.001 { // Use small epsilon for floating point comparison
+            // Stable tie-breaker: use index as secondary sort key
+            return $0.index < $1.index
+        }
+        return projection1 > projection2 // Sort in descending order (largest projection first)
+    }
 
     for floating in floatingWindows { // Make floating windows be seen as tiling
         floating.window.bind(to: floating.parent, adaptiveWeight: 1, index: floating.index)

yuliswe/Devs-Best-AeroSpace-fork

Sources/AppBundle/command/impl/FocusCommand.swift
@@ -127,6 +127,15 @@ struct FocusCommand: Command {
     defer {
         mruBefore?.markAsMostRecentChild()
     }
+    
+    // Get the first tiling window in the workspace as the target
+    guard let firstTilingWindow = workspace.rootTilingContainer.allLeafWindowsRecursive.first else {
+        return []
+    }
+    guard let tilingParent = firstTilingWindow.parent as? TilingContainer else {
+        return []
+    }
+    
     var _floatingWindows: [FloatingWindowData] = []
     for window in workspace.floatingWindows {
         // todo bug: we shouldn't access ax api here. What if the window was moved but it wasn't committed to ax yet?
@@ -165,7 +174,15 @@ struct FocusCommand: Command {
         )
         _floatingWindows.append(floatingWindowData)
     }
-    let floatingWindows: [FloatingWindowData] = _floatingWindows.sortedBy { $0.center.getProjection($0.tilingParent.orientation) }.reversed()
+    let floatingWindows: [FloatingWindowData] = _floatingWindows.sorted {
+        let projection1 = $0.center.getProjection($0.tilingParent.orientation)
+        let projection2 = $1.center.getProjection($1.tilingParent.orientation)
+        if abs(projection1 - projection2) < 0.001 { // Use small epsilon for floating point comparison
+            // Stable tie-breaker: use index as secondary sort key
+            return $0.index < $1.index
+        }
+        return projection1 > projection2 // Sort in descending order (largest projection first)
+    }
 
     for floating in floatingWindows { // Make floating windows be seen as tiling
         floating.window.bind(to: floating.tilingParent, adaptiveWeight: 1, index: floating.index)